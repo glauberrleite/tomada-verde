ede0bae87afe836a7ae936ae5d0026be5619c04f
glauberrleite <contact@glauberrleite.com>
2018-08-16 14:14:42 -0300

    Working version

--- main.py
+++ main.py
@@ -1,25 +1,26 @@
-#!/usr/bin/env python
-from PlugPower import PlugPower
+import plug_power
 import time
-
-outletPin = 4
-lightPin = 17
-
+import pigpio
+#Olhar os Pinos
+#Pin1 = Inversor
+#Pin2 = Lampada
+#Double check o plug_power pls
+pin1 = 4
+pin2 = 17
 def check_power(pp):
+	pi = pigpio.pi()
+	pi.set_mode(pin1, pigpio.OUTPUT)
+	pi.set_mode(pin2, pigpio.OUTPUT)
 	pp.check_hour()
-    pi.write(pin1, pp.state1)
-    pi.write(pin2, pp.state2)
-    while True:
-        time.sleep(300)
+	pi.write(pin1, pp.state1)
+	pi.write(pin2, pp.state2)
+	while True:
+        	time.sleep(300)
 		pp.check_hour()
-        if pp.state_change1:
-			#Arrumar Aqui
-            pi.write(pin1, pp.state1)
+        	if pp.state_change1:
+            		pi.write(pin1, pp.state1)
 		if pp.state_change2:
-			#Arrumar Aqui
 			pi.write(pin2, pp.state2)
 
-plug = PlugPower(outletPin, lightPin)
-
-while True:
-	plug.check()
+plug = plug_power.plug_power()
+check_power(plug)
--- plug_power.py
+++ plug_power.py
@@ -1,20 +1,30 @@
 from datetime import datetime
 class plug_power:
 	def __init__(self):
-		self.state = 0 #0 = off; 1 = on
+		self.state1 = 0 #0 = off; 1 = on
+		self.state2 = 0
+		self.state_change1 = 0
+		self.state_change2 = 0
 		self.date = datetime.now()
 
 	def check_hour(self):
 		self.date = datetime.now()
-		self.state_change = 0
-		
-		if (self.date.hour >= 19) | ((self.date.hour >= 12) & (self.date.weekday() == 5)):
-			if (self.state == 1):
-				self.state = 0
-				self.state_change = 1
-		elif (((self.date.hour >= 7) & (self.date.minute >= 30)) | (self.date.hour >= 8)) & (self.date.weekday() <= 5):
-			if (self.state == 0): 
-				self.state = 1
-				self.state_change = 1
+		self.state_change1 = 0
+		self.state_change2 = 0
+		if (self.date.hour >= 19) or ((self.date.hour >= 12) and (self.date.weekday() == 5)):
+			if (self.state1 == 1):
+				self.state1 = 0
+				self.state_change1 = 1
+		elif ((self.date.hour >= 7) and (self.date.weekday() <= 5)):
+			if (self.state1 == 0):
+				self.state1 = 1
+				self.state_change1 = 1
+		if (self.date.hour >= 18):
+			if(self.state2 == 0):
+				self.state2 = 1
+				self.state_change2 = 1
+		elif (self.date.hour >= 4):
+			if(self.state2 == 1):
+				self.state2 = 0
+				self.state_change2 = 1
 
-		return self.state_change
